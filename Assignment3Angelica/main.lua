582b7ceab08b52f37f1a48e23bf0ccd8f5174259
Angelica Lutkiewicz <s290747@ocsb.local>
2018-09-27 14:30:18 -0400

    School

--- Assignment3Angelica/main.lua
+++ Assignment3Angelica/main.lua
@@ -11,10 +11,15 @@ local triangleHalfH = display.contentHeight * 0.2
 local verticesT = { -250,100, -50,100, -150,-80}
 local triangle = display.newPolygon( triangleHalfW, triangleHalfH, verticesT )
 --
-local rhombus = display.newPolygon( rhombusHalfW, rhombusHalfH, verticesR )
-local verticesR = { -300,300, -10,300, -160,10, -20,-80}
-local rhombusHalfW = display.contentWidth * 0.2
-local rhombusHalfH = display.contentWidth * 0.3
+local halfW = display.contentWidth * 0.7
+local halfH = display.contentHeight * 0.2
+local vertices = { -250,100, -50,100, -175,-80, }
+local rhombus = display.newPolygon( halfW, halfH, vertices )
+
+--local rhombus = display.newPolygon( rhombusHalfW, rhombusHalfH, verticesR )
+--local verticesR = { -300,300, -10,300, -160,10, -20,-80}
+--local rhombusHalfW = display.contentWidth * 0.2
+--local rhombusHalfH = display.contentWidth * 0.3
 
 -- Sets the background colour of the screen
 display.setDefault("background", 1, 0, 0)
@@ -29,9 +34,13 @@ triangle:setFillColor(0, 1, 0)
 triangle.strokeWidth = 10
 triangle:setStrokeColor( 0 )
 
+-- Rhombus Fill
+rhombus:setFillColor(0)
+
 -- local rhombus = display.newPolygon( rhombusHalfW, rhombusHalfH, verticesR )
 -- local verticesR = { -300,300, -10,300, -160,10, 0,1}
 -- local rhombusHalfW = display.contentWidth * 0.5
 -- local rhombusHalfH = display.contentWidth * 0.5
 
 -- triangleHalfW, triangleHalfH, 
+-- 27,-35, 105,-35, 43,16, 65,90, 0,45, -65,90, -43,15, -105,-35, -27,-35, }
