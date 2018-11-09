a1242b5de7e525deeeff0f1ab57c6ee045edbe83
AngelicaL714 <angelica.lutkiewicz@ocsbstudent.ca>
2018-11-09 14:20:46 -0500

    code

--- CompanyLogoAnimationAngelicaL/main.lua
+++ CompanyLogoAnimationAngelicaL/main.lua
@@ -14,8 +14,14 @@ display.setDefault("background", 154/255, 153/255, 255/255)
 -----------------------------------------------------------------------------------------
 -- Local Variables
 -----------------------------------------------------------------------------------------
+	-- Banana
 local banana = display.newImageRect("Images/CompanyLogoAngelica@2xCopy.png", display.contentWidth/5, display.contentHeight/5 + display.contentHeight/14)
 local bananaWidth = banana.width
 local bananaHeight = banana.height
+	-- Placement of the banana
 banana.x = display.contentWidth *2/4
 banana.y = display.contentHeight *2/4
+
+-----------------------------------------------------------------------------------------
+-- Object Creation
+-----------------------------------------------------------------------------------------
