eb40f376646d6a8145a7a41b69d5eb43cc5728a4
AngelicaHome <angelica.lutkiewicz@ocsbstudent.ca>
2018-11-12 23:35:08 -0500

    Update2/Finished

--- CompanyLogoAnimationAngelicaL/main.lua
+++ CompanyLogoAnimationAngelicaL/main.lua
@@ -8,12 +8,13 @@
 -- Hides the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+-- Sets the background image/colour
 display.setDefault("background", 154/255, 153/255, 255/255)
-
 
 -----------------------------------------------------------------------------------------
 -- Local Variables
 -----------------------------------------------------------------------------------------
+
 	-- Banana
 local banana = display.newImageRect("Images/CompanyLogoAngelica@2xCopy.png", display.contentWidth/3, display.contentHeight/3 + display.contentHeight/12)
 -- Sets the width and height of the banana
@@ -24,7 +25,7 @@ banana.x = display.contentWidth *2/4
 banana.y = display.contentHeight *3/4
 
 	-- Company Name
-local Jojo = display.newImageRect("Images/CompanyLogoTextAngelicaCopy@2x.png",  display.contentWidth*11/13, display.contentHeight/4 + display.contentHeight/20)
+local Jojo=display.newImageRect("Images/CompanyLogoTextAngelicaCopy@2x.png",display.contentWidth*11/13,display.contentHeight/4+display.contentHeight/20)
 -- Sets the heighht and width of the company name
 local JojoWidth = Jojo.width
 local JojoHeight = Jojo.height
@@ -34,41 +35,46 @@ Jojo.y = display.contentHeight*3/4
 -- sets the transparency
 Jojo.alpha = 0
 
-
 -- Rate of which the image moves
 scrollSpeed = 4
 stop = 0
+
 -----------------------------------------------------------------------------------------
 -- Local functions
 -----------------------------------------------------------------------------------------
 local function StopBanana()
- if	(banana.y == display.contentHeight/4) then
- 	-- sets the rate of which the banana moves to 0 so it stops
- 	scrollSpeed = stop
- end
 
+	if (banana.y == display.contentHeight/4) then
+ 		-- sets the rate of which the banana moves to 0 so it stops
+ 		scrollSpeed = stop
+ 	end
 end
 
-
+-- This function moves the banana displayed
 local function MoveBanana( event )
+
 	-- Add the scroll speed to the banana so it moves vertically
 	banana.y = banana.y - scrollSpeed
 	-- Calls function StopBanana
 	StopBanana()
 end
 
+-- This function fades the company logo from being trasparent to being opaque
 local function FadeInName()
+
+	-- Changes the alpha
 	Jojo.alpha = Jojo.alpha + 0.008
 end
 
 -----------------------------------------------------------------------------------------
 -- Event Listeners
 -----------------------------------------------------------------------------------------
+
 -- MoveBanana fuction
 Runtime:addEventListener("enterFrame", MoveBanana)
 Runtime:addEventListener("enterFrame", FadeInName)
+
 -- Calls the function
 MoveBanana()
 FadeInName()
 
-	
